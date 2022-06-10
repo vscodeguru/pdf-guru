67dab111cf3d7c8cd46d5f2b4df043c3a541441f
jones <ansolamjones1902@gmail.com>
2022-06-10 21:00:38 +0530

    Command Removed

--- src/PdfSharp/Pdf/KeysMeta.cs
+++ src/PdfSharp/Pdf/KeysMeta.cs
@@ -1,32 +1,3 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
@@ -34,16 +5,8 @@ using System.Reflection;
 
 namespace PdfSharp.Pdf
 {
-    /// <summary>
-    /// Holds information about the value of a key in a dictionary. This information is used to create
-    /// and interpret this value.
-    /// </summary>
     internal sealed class KeyDescriptor
     {
-        /// <summary>
-        /// Initializes a new instance of KeyDescriptor from the specified attribute during a KeysMeta
-        /// initializes itself using reflection.
-        /// </summary>
         public KeyDescriptor(KeyInfoAttribute attribute)
         {
             _version = attribute.Version;
@@ -55,9 +18,6 @@ namespace PdfSharp.Pdf
                 _version = "1.0";
         }
 
-        /// <summary>
-        /// Gets or sets the PDF version starting with the availability of the described key.
-        /// </summary>
         public string Version
         {
             get { return _version; }
@@ -97,15 +57,11 @@ namespace PdfSharp.Pdf
             get { return (_keyType & KeyType.MustNotBeIndirect) == 0; }
         }
 
-        /// <summary>
-        /// Returns the type of the object to be created as value for the described key.
-        /// </summary>
         public Type GetValueType()
         {
             Type type = _objectType;
             if (type == null)
             {
-                // If we have no ObjectType specified, use the KeyType enumeration.
                 switch (_keyType & KeyType.TypeMask)
                 {
                     case KeyType.Name:
@@ -148,8 +104,6 @@ namespace PdfSharp.Pdf
                         type = typeof(PdfDictionary);
                         break;
 
-                    // The following types are not yet used
-
                     case KeyType.NumberTree:
                         throw new NotImplementedException("KeyType.NumberTree");
 
@@ -163,9 +117,7 @@ namespace PdfSharp.Pdf
                         throw new NotImplementedException("KeyType.StreamOrArray");
 
                     case KeyType.ArrayOrNameOrString:
-                        return null; // HACK: Make PdfOutline work
-                                     //throw new NotImplementedException("KeyType.ArrayOrNameOrString");
-
+                        return null;     
                     default:
                         Debug.Assert(false, "Invalid KeyType: " + _keyType);
                         break;
@@ -175,24 +127,10 @@ namespace PdfSharp.Pdf
         }
     }
 
-    /// <summary>
-    /// Contains meta information about all keys of a PDF dictionary.
-    /// </summary>
     internal class DictionaryMeta
     {
         public DictionaryMeta(Type type)
         {
-            //#if (NETFX_CORE && DEBUG) || CORE
-            //            if (type == typeof(PdfPages.Keys))
-            //            {
-            //                var x = typeof(PdfPages).GetRuntimeFields();
-            //                var y = typeof(PdfPages).GetTypeInfo().DeclaredFields;
-            //                x.GetType();
-            //                y.GetType();
-            //                Debug-Break.Break();
-            //                Test.It();
-            //            }
-            //#endif
 #if !NETFX_CORE && !UWP
             FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
             foreach (FieldInfo field in fields)
@@ -206,86 +144,9 @@ namespace PdfSharp.Pdf
                     _keyDescriptors[descriptor.KeyValue] = descriptor;
                 }
             }
-#else
-            // Rewritten for WinRT.
-            CollectKeyDescriptors(type);
-            //var fields = type.GetRuntimeFields();  // does not work
-            //fields2.GetType();
-            //foreach (FieldInfo field in fields)
-            //{
-            //    var attributes = field.GetCustomAttributes(typeof(KeyInfoAttribute), false);
-            //    foreach (var attribute in attributes)
-            //    {
-            //        KeyDescriptor descriptor = new KeyDescriptor((KeyInfoAttribute)attribute);
-            //        descriptor.KeyValue = (string)field.GetValue(null);
-            //        _keyDescriptors[descriptor.KeyValue] = descriptor;
-            //    }
-            //}
 #endif
         }
 
-#if NETFX_CORE || UWP
-        // Background: The function GetRuntimeFields gets constant fields only for the specified type,
-        // not for its base types. So we have to walk recursively through base classes.
-        // The docmentation says full trust for the immediate caller is required for property BaseClass.
-        // TODO: Rewrite this stuff for medium trust.
-        void CollectKeyDescriptors(Type type)
-        {
-            // Get fields of the specified type only.
-            var fields = type.GetTypeInfo().DeclaredFields;
-            foreach (FieldInfo field in fields)
-            {
-                var attributes = field.GetCustomAttributes(typeof(KeyInfoAttribute), false);
-                foreach (var attribute in attributes)
-                {
-                    KeyDescriptor descriptor = new KeyDescriptor((KeyInfoAttribute)attribute);
-                    descriptor.KeyValue = (string)field.GetValue(null);
-                    _keyDescriptors[descriptor.KeyValue] = descriptor;
-                }
-            }
-            type = type.GetTypeInfo().BaseType;
-            if (type != typeof(object) && type != typeof(PdfObject))
-                CollectKeyDescriptors(type);
-        }
-#endif
-
-#if (NETFX_CORE || CORE) && true_
-        public class A
-        {
-            public string _a;
-            public const string _ca = "x";
-        }
-        public class B : A
-        {
-            public string _b;
-            public const string _cb = "x";
-
-            void Foo()
-            {
-                var str = A._ca;
-            }
-        }
-        class Test
-        {
-            public static void It()
-            {
-                string s = "Runtime fields of B:";
-                foreach (var fieldInfo in typeof(B).GetRuntimeFields()) { s += " " + fieldInfo.Name; }
-                Debug.WriteLine(s);
-
-                s = "Declared fields of B:";
-                foreach (var fieldInfo in typeof(B).GetTypeInfo().DeclaredFields) { s += " " + fieldInfo.Name; }
-                Debug.WriteLine(s);
-
-                s = "Runtime fields of PdfPages.Keys:";
-                foreach (var fieldInfo in typeof(PdfPages.Keys).GetRuntimeFields()) { s += " " + fieldInfo.Name; }
-                Debug.WriteLine(s);
-            }
-        }
-#endif
-        /// <summary>
-        /// Gets the KeyDescriptor of the specified key, or null if no such descriptor exits.
-        /// </summary>
         public KeyDescriptor this[string key]
         {
             get
--- src/PdfSharp/Pdf/PdfArray.cs
+++ src/PdfSharp/Pdf/PdfArray.cs
@@ -1,32 +1,3 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -38,31 +9,16 @@ using PdfSharp.Pdf.IO;
 
 namespace PdfSharp.Pdf
 {
-    /// <summary>
-    /// Represents a PDF array object.
-    /// </summary>
     [DebuggerDisplay("{DebuggerDisplay}")]
     public class PdfArray : PdfObject, IEnumerable<PdfItem>
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfArray"/> class.
-        /// </summary>
         public PdfArray()
         { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfArray"/> class.
-        /// </summary>
-        /// <param name="document">The document.</param>
         public PdfArray(PdfDocument document)
             : base(document)
         { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfArray"/> class.
-        /// </summary>
-        /// <param name="document">The document.</param>
-        /// <param name="items">The items.</param>
         public PdfArray(PdfDocument document, params PdfItem[] items)
             : base(document)
         {
@@ -70,10 +26,6 @@ namespace PdfSharp.Pdf
                 Elements.Add(item);
         }
 
-        /// <summary>
-        /// Initializes a new instance from an existing dictionary. Used for object type transformation.
-        /// </summary>
-        /// <param name="array">The array.</param>
         protected PdfArray(PdfArray array)
             : base(array)
         {
@@ -81,18 +33,11 @@ namespace PdfSharp.Pdf
                 array._elements.ChangeOwner(this);
         }
 
-        /// <summary>
-        /// Creates a copy of this array. Direct elements are deep copied.
-        /// Indirect references are not modified.
-        /// </summary>
         public new PdfArray Clone()
         {
             return (PdfArray)Copy();
         }
 
-        /// <summary>
-        /// Implements the copy mechanism.
-        /// </summary>
         protected override object Copy()
         {
             PdfArray array = (PdfArray)base.Copy();
@@ -110,17 +55,11 @@ namespace PdfSharp.Pdf
             return array;
         }
 
-        /// <summary>
-        /// Gets the collection containing the elements of this object.
-        /// </summary>
         public ArrayElements Elements
         {
             get { return _elements ?? (_elements = new ArrayElements(this)); }
         }
 
-        /// <summary>
-        /// Returns an enumerator that iterates through a collection.
-        /// </summary>
         public virtual IEnumerator<PdfItem> GetEnumerator()
         {
             return Elements.GetEnumerator();
@@ -131,9 +70,6 @@ namespace PdfSharp.Pdf
             return GetEnumerator();
         }
 
-        /// <summary>
-        /// Returns a string with the content of this object in a readable form. Useful for debugging purposes only.
-        /// </summary>
         public override string ToString()
         {
             StringBuilder pdf = new StringBuilder();
@@ -157,9 +93,6 @@ namespace PdfSharp.Pdf
             writer.WriteEndObject();
         }
 
-        /// <summary>
-        /// Represents the elements of an PdfArray.
-        /// </summary>
         public sealed class ArrayElements : IList<PdfItem>, ICloneable
         {
             internal ArrayElements(PdfArray array)
@@ -176,37 +109,22 @@ namespace PdfSharp.Pdf
                 return elements;
             }
 
-            /// <summary>
-            /// Creates a shallow copy of this object.
-            /// </summary>
             public ArrayElements Clone()
             {
                 return (ArrayElements)((ICloneable)this).Clone();
             }
 
-            /// <summary>
-            /// Moves this instance to another array during object type transformation.
-            /// </summary>
             internal void ChangeOwner(PdfArray array)
             {
                 if (_ownerArray != null)
                 {
-                    // ???
                 }
 
-                // Set new owner.
                 _ownerArray = array;
 
-                // Set owners elements to this.
                 array._elements = this;
             }
 
-            /// <summary>
-            /// Converts the specified value to boolean.
-            /// If the value does not exist, the function returns false.
-            /// If the value is not convertible, the function throws an InvalidCastException.
-            /// If the index is out of range, the function throws an ArgumentOutOfRangeException.
-            /// </summary>
             public bool GetBoolean(int index)
             {
                 if (index < 0 || index >= Count)
@@ -227,12 +145,6 @@ namespace PdfSharp.Pdf
                 throw new InvalidCastException("GetBoolean: Object is not a boolean.");
             }
 
-            /// <summary>
-            /// Converts the specified value to integer.
-            /// If the value does not exist, the function returns 0.
-            /// If the value is not convertible, the function throws an InvalidCastException.
-            /// If the index is out of range, the function throws an ArgumentOutOfRangeException.
-            /// </summary>
             public int GetInteger(int index)
             {
                 if (index < 0 || index >= Count)
@@ -253,12 +165,6 @@ namespace PdfSharp.Pdf
                 throw new InvalidCastException("GetInteger: Object is not an integer.");
             }
 
-            /// <summary>
-            /// Converts the specified value to double.
-            /// If the value does not exist, the function returns 0.
-            /// If the value is not convertible, the function throws an InvalidCastException.
-            /// If the index is out of range, the function throws an ArgumentOutOfRangeException.
-            /// </summary>
             public double GetReal(int index)
             {
                 if (index < 0 || index >= Count)
@@ -287,12 +193,6 @@ namespace PdfSharp.Pdf
                 throw new InvalidCastException("GetReal: Object is not a number.");
             }
 
-            /// <summary>
-            /// Converts the specified value to double?.
-            /// If the value does not exist, the function returns null.
-            /// If the value is not convertible, the function throws an InvalidCastException.
-            /// If the index is out of range, the function throws an ArgumentOutOfRangeException.
-            /// </summary>
             public double? GetNullableReal(int index)
             {
                 if (index < 0 || index >= Count)
@@ -329,12 +229,6 @@ namespace PdfSharp.Pdf
                 throw new InvalidCastException("GetReal: Object is not a number.");
             }
 
-            /// <summary>
-            /// Converts the specified value to string.
-            /// If the value does not exist, the function returns the empty string.
-            /// If the value is not convertible, the function throws an InvalidCastException.
-            /// If the index is out of range, the function throws an ArgumentOutOfRangeException.
-            /// </summary>
             public string GetString(int index)
             {
                 if (index < 0 || index >= Count)
@@ -355,12 +249,6 @@ namespace PdfSharp.Pdf
                 throw new InvalidCastException("GetString: Object is not a string.");
             }
 
-            /// <summary>
-            /// Converts the specified value to a name.
-            /// If the value does not exist, the function returns the empty string.
-            /// If the value is not convertible, the function throws an InvalidCastException.
-            /// If the index is out of range, the function throws an ArgumentOutOfRangeException.
-            /// </summary>
             public string GetName(int index)
             {
                 if (index < 0 || index >= Count)
@@ -381,9 +269,6 @@ namespace PdfSharp.Pdf
                 throw new InvalidCastException("GetName: Object is not a name.");
             }
 
-            /// <summary>
-            /// Returns the indirect object if the value at the specified index is a PdfReference.
-            /// </summary>
             [Obsolete("Use GetObject, GetDictionary, GetArray, or GetReference")]
             public PdfObject GetIndirectObject(int index)
             {
@@ -397,10 +282,6 @@ namespace PdfSharp.Pdf
                 return null;
             }
 
-            /// <summary>
-            /// Gets the PdfObject with the specified index, or null, if no such object exists. If the index refers to
-            /// a reference, the referenced PdfObject is returned.
-            /// </summary>
             public PdfObject GetObject(int index)
             {
                 if (index < 0 || index >= Count)
@@ -414,55 +295,32 @@ namespace PdfSharp.Pdf
                 return item as PdfObject;
             }
 
-            /// <summary>
-            /// Gets the PdfArray with the specified index, or null, if no such object exists. If the index refers to
-            /// a reference, the referenced PdfArray is returned.
-            /// </summary>
             public PdfDictionary GetDictionary(int index)
             {
                 return GetObject(index) as PdfDictionary;
             }
 
-            /// <summary>
-            /// Gets the PdfArray with the specified index, or null, if no such object exists. If the index refers to
-            /// a reference, the referenced PdfArray is returned.
-            /// </summary>
             public PdfArray GetArray(int index)
             {
                 return GetObject(index) as PdfArray;
             }
 
-            /// <summary>
-            /// Gets the PdfReference with the specified index, or null, if no such object exists.
-            /// </summary>
             public PdfReference GetReference(int index)
             {
                 PdfItem item = this[index];
                 return item as PdfReference;
             }
 
-            /// <summary>
-            /// Gets all items of this array.
-            /// </summary>
             public PdfItem[] Items
             {
                 get { return _elements.ToArray(); }
             }
 
-            #region IList Members
-
-            /// <summary>
-            /// Returns false.
-            /// </summary>
             public bool IsReadOnly
             {
                 get { return false; }
             }
 
-            /// <summary>
-            /// Gets or sets an item at the specified index.
-            /// </summary>
-            /// <value></value>
             public PdfItem this[int index]
             {
                 get { return _elements[index]; }
@@ -474,63 +332,38 @@ namespace PdfSharp.Pdf
                 }
             }
 
-            /// <summary>
-            /// Removes the item at the specified index.
-            /// </summary>
             public void RemoveAt(int index)
             {
                 _elements.RemoveAt(index);
             }
 
-            /// <summary>
-            /// Removes the first occurrence of a specific object from the array/>.
-            /// </summary>
             public bool Remove(PdfItem item)
             {
                 return _elements.Remove(item);
             }
 
-            /// <summary>
-            /// Inserts the item the specified index.
-            /// </summary>
             public void Insert(int index, PdfItem value)
             {
                 _elements.Insert(index, value);
             }
 
-            /// <summary>
-            /// Determines whether the specified value is in the array.
-            /// </summary>
             public bool Contains(PdfItem value)
             {
                 return _elements.Contains(value);
             }
 
-            /// <summary>
-            /// Removes all items from the array.
-            /// </summary>
             public void Clear()
             {
                 _elements.Clear();
             }
 
-            /// <summary>
-            /// Gets the index of the specified item.
-            /// </summary>
             public int IndexOf(PdfItem value)
             {
                 return _elements.IndexOf(value);
             }
 
-            /// <summary>
-            /// Appends the specified object to the array.
-            /// </summary>
             public void Add(PdfItem value)
             {
-                // TODO: ??? 
-                //Debug.Assert((value is PdfObject && ((PdfObject)value).Reference == null) | !(value is PdfObject),
-                //  "You try to set an indirect object directly into an array.");
-
                 PdfObject obj = value as PdfObject;
                 if (obj != null && obj.IsIndirect)
                     _elements.Add(obj.Reference);
@@ -538,55 +371,31 @@ namespace PdfSharp.Pdf
                     _elements.Add(value);
             }
 
-            /// <summary>
-            /// Returns false.
-            /// </summary>
             public bool IsFixedSize
             {
                 get { return false; }
             }
 
-            #endregion
-
-            #region ICollection Members
-
-            /// <summary>
-            /// Returns false.
-            /// </summary>
             public bool IsSynchronized
             {
                 get { return false; }
             }
 
-            /// <summary>
-            /// Gets the number of elements in the array.
-            /// </summary>
             public int Count
             {
                 get { return _elements.Count; }
             }
 
-            /// <summary>
-            /// Copies the elements of the array to the specified array.
-            /// </summary>
             public void CopyTo(PdfItem[] array, int index)
             {
                 _elements.CopyTo(array, index);
             }
 
-            /// <summary>
-            /// The current implementation return null.
-            /// </summary>
             public object SyncRoot
             {
                 get { return null; }
             }
 
-            #endregion
-
-            /// <summary>
-            /// Returns an enumerator that iterates through the array.
-            /// </summary>
             public IEnumerator<PdfItem> GetEnumerator()
             {
                 return _elements.GetEnumerator();
@@ -597,31 +406,18 @@ namespace PdfSharp.Pdf
                 return _elements.GetEnumerator();
             }
 
-            /// <summary>
-            /// The elements of the array.
-            /// </summary>
             List<PdfItem> _elements;
 
-            /// <summary>
-            /// The array this objects belongs to.
-            /// </summary>
             PdfArray _ownerArray;
         }
 
         ArrayElements _elements;
 
-        /// <summary>
-        /// Gets the DebuggerDisplayAttribute text.
-        /// </summary>
-        // ReSharper disable UnusedMember.Local
         string DebuggerDisplay
-        // ReSharper restore UnusedMember.Local
         {
             get
             {
 #if true
-                return String.Format(CultureInfo.InvariantCulture, "array({0},[{1}])", ObjectID.DebuggerDisplay, _elements == null ? 0 : _elements.Count);
-#else
                 return String.Format(CultureInfo.InvariantCulture, "array({0},[{1}])", ObjectID.DebuggerDisplay, _elements == null ? 0 : _elements.Count);
 #endif
             }
