cb87d314f165fe976b8082279c354f701b60977a
jones <ansolamjones1902@gmail.com>
2022-06-10 21:42:52 +0530

    Command Removed

--- src/PdfSharp/Pdf.AcroForms/PdfButtonField.cs
+++ src/PdfSharp/Pdf.AcroForms/PdfButtonField.cs
@@ -1,32 +1,3 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -34,33 +5,18 @@ using PdfSharp.Pdf.Annotations;
 
 namespace PdfSharp.Pdf.AcroForms
 {
-    /// <summary>
-    /// Represents the base class for all button fields.
-    /// </summary>
     public abstract class PdfButtonField : PdfAcroField
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfButtonField"/> class.
-        /// </summary>
         protected PdfButtonField(PdfDocument document)
             : base(document)
         { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfButtonField"/> class.
-        /// </summary>
         protected PdfButtonField(PdfDictionary dict)
             : base(dict)
         { }
 
-        /// <summary>
-        /// Gets the name which represents the opposite of /Off.
-        /// </summary>
         protected string GetNonOffValue()
         {
-            // Try to get the information from the appearance dictionaray.
-            // Just return the first key that is not /Off.
-            // I'm not sure what is the right solution to get this value.
             PdfDictionary ap = Elements[PdfAnnotation.Keys.AP] as PdfDictionary;
             if (ap != null)
             {
@@ -78,7 +34,6 @@ namespace PdfSharp.Pdf.AcroForms
         internal override void GetDescendantNames(ref List<string> names, string partialName)
         {
             string t = Elements.GetString(PdfAcroField.Keys.T);
-            // HACK: ??? 
             if (t == "")
                 t = "???";
             Debug.Assert(t != "");
@@ -91,13 +46,8 @@ namespace PdfSharp.Pdf.AcroForms
             }
         }
 
-        /// <summary>
-        /// Predefined keys of this dictionary. 
-        /// The description comes from PDF 1.4 Reference.
-        /// </summary>
         public new class Keys : PdfAcroField.Keys
         {
-            // Pushbuttons have no additional entries.
         }
     }
 }
--- src/PdfSharp/Pdf.AcroForms/PdfComboBoxField.cs
+++ src/PdfSharp/Pdf.AcroForms/PdfComboBoxField.cs
@@ -1,44 +1,9 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 
 namespace PdfSharp.Pdf.AcroForms
 {
-    /// <summary>
-    /// Represents the combo box field.
-    /// </summary>
     public sealed class PdfComboBoxField : PdfChoiceField
     {
-        /// <summary>
-        /// Initializes a new instance of PdfComboBoxField.
-        /// </summary>
         internal PdfComboBoxField(PdfDocument document)
             : base(document)
         { }
@@ -47,9 +12,6 @@ namespace PdfSharp.Pdf.AcroForms
             : base(dict)
         { }
 
-        /// <summary>
-        /// Gets or sets the index of the selected item.
-        /// </summary>
         public int SelectedIndex
         {
             get
@@ -59,21 +21,16 @@ namespace PdfSharp.Pdf.AcroForms
             }
             set
             {
-                // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx		  
-                if (value != -1) //R080325
+                if (value != -1) 
                 {
                     string key = ValueInOptArray(value);
                     Elements.SetString(Keys.V, key);
-                    Elements.SetInteger("/I", value); //R080304 !!!!!!! sonst reagiert die Combobox überhaupt nicht !!!!!
+                    Elements.SetInteger("/I", value);         
                 }
             }
         }
 
-        /// <summary>
-        /// Gets or sets the value of the field.
-        /// </summary>
-        // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx		  
-        public override PdfItem Value //R080304
+        public override PdfItem Value 
         {
             get { return Elements[Keys.V]; }
             set
@@ -83,13 +40,11 @@ namespace PdfSharp.Pdf.AcroForms
                 if (value is PdfString || value is PdfName)
                 {
                     Elements[Keys.V] = value;
-                    SelectedIndex = SelectedIndex; //R080304 !!!
+                    SelectedIndex = SelectedIndex;  
                     if (SelectedIndex == -1)
                     {
-                        //R080317 noch nicht rund
                         try
                         {
-                            //anhängen
                             ((PdfArray)(((PdfItem[])(Elements.Values))[2])).Elements.Add(Value);
                             SelectedIndex = SelectedIndex;
                         }
@@ -101,14 +56,8 @@ namespace PdfSharp.Pdf.AcroForms
             }
         }
 
-        /// <summary>
-        /// Predefined keys of this dictionary. 
-        /// The description comes from PDF 1.4 Reference.
-        /// </summary>
         public new class Keys : PdfAcroField.Keys
         {
-            // Combo boxes have no additional entries.
-
             internal static DictionaryMeta Meta
             {
                 get
@@ -121,9 +70,6 @@ namespace PdfSharp.Pdf.AcroForms
             static DictionaryMeta _meta;
         }
 
-        /// <summary>
-        /// Gets the KeysMeta of this dictionary type.
-        /// </summary>
         internal override DictionaryMeta Meta
         {
             get { return Keys.Meta; }
