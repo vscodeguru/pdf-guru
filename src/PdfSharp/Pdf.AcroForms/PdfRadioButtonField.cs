8f5caaaf7afc51dcc63e888db7714c6be63095c9
jones <ansolamjones1902@gmail.com>
2022-06-10 21:45:33 +0530

    Command Removed

--- src/PdfSharp/Pdf.AcroForms/PdfRadioButtonField.cs
+++ src/PdfSharp/Pdf.AcroForms/PdfRadioButtonField.cs
@@ -1,44 +1,9 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 
 namespace PdfSharp.Pdf.AcroForms
 {
-    /// <summary>
-    /// Represents the radio button field.
-    /// </summary>
     public sealed class PdfRadioButtonField : PdfButtonField
     {
-        /// <summary>
-        /// Initializes a new instance of PdfRadioButtonField.
-        /// </summary>
         internal PdfRadioButtonField(PdfDocument document)
             : base(document)
         {
@@ -49,9 +14,6 @@ namespace PdfSharp.Pdf.AcroForms
             : base(dict)
         { }
 
-        /// <summary>
-        /// Gets or sets the index of the selected radio button in a radio button group.
-        /// </summary>
         public int SelectedIndex
         {
             get
@@ -95,25 +57,11 @@ namespace PdfSharp.Pdf.AcroForms
             return -1;
         }
 
-        /// <summary>
-        /// Predefined keys of this dictionary. 
-        /// The description comes from PDF 1.4 Reference.
-        /// </summary>
         public new class Keys : PdfButtonField.Keys
         {
-            /// <summary>
-            /// (Optional; inheritable; PDF 1.4) An array of text strings to be used in
-            /// place of the V entries for the values of the widget annotations representing
-            /// the individual radio buttons. Each element in the array represents
-            /// the export value of the corresponding widget annotation in the
-            /// Kids array of the radio button field.
-            /// </summary>
             [KeyInfo(KeyType.Array | KeyType.Optional)]
             public const string Opt = "/Opt";
 
-            /// <summary>
-            /// Gets the KeysMeta for these keys.
-            /// </summary>
             internal static DictionaryMeta Meta
             {
                 get { return _meta ?? (_meta = CreateMeta(typeof(Keys))); }
@@ -121,9 +69,6 @@ namespace PdfSharp.Pdf.AcroForms
             static DictionaryMeta _meta;
         }
 
-        /// <summary>
-        /// Gets the KeysMeta of this dictionary type.
-        /// </summary>
         internal override DictionaryMeta Meta
         {
             get { return Keys.Meta; }
