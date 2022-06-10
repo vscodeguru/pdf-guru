5610aadb632a78ab4aca63ad3d347800132e265d
jones <ansolamjones1902@gmail.com>
2022-06-10 11:47:30 +0530

    Command Removed

--- src/PdfSharp/Drawing/FontFamilyCache.cs
+++ src/PdfSharp/Drawing/FontFamilyCache.cs
@@ -1,32 +1,3 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -49,9 +20,6 @@ using PdfSharp.Pdf;
 
 namespace PdfSharp.Drawing
 {
-    /// <summary>
-    /// Global cache of all internal font family objects.
-    /// </summary>
     internal sealed class FontFamilyCache
     {
         FontFamilyCache()
@@ -71,15 +39,11 @@ namespace PdfSharp.Drawing
             finally { Lock.ExitFontFactory(); }
         }
 
-        /// <summary>
-        /// Caches the font family or returns a previously cached one.
-        /// </summary>
         public static FontFamilyInternal CacheOrGetFontFamily(FontFamilyInternal fontFamily)
         {
             try
             {
                 Lock.EnterFontFactory();
-                // Recall that a font family is uniquely identified by its case insensitive name.
                 FontFamilyInternal existingFontFamily;
                 if (Singleton._familiesByName.TryGetValue(fontFamily.Name, out existingFontFamily))
                 {
@@ -95,14 +59,10 @@ namespace PdfSharp.Drawing
             finally { Lock.ExitFontFactory(); }
         }
 
-        /// <summary>
-        /// Gets the singleton.
-        /// </summary>
         static FontFamilyCache Singleton
         {
             get
             {
-                // ReSharper disable once InvertIf
                 if (_singleton == null)
                 {
                     try
@@ -134,9 +94,6 @@ namespace PdfSharp.Drawing
             return state.ToString();
         }
 
-        /// <summary>
-        /// Maps family name to internal font family.
-        /// </summary>
         readonly Dictionary<string, FontFamilyInternal> _familiesByName;
     }
 }
