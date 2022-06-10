aaa4339326671325fd81debe9f91ac2ca133f8b2
jones <ansolamjones1902@gmail.com>
2022-06-10 11:46:20 +0530

    Command Removed

--- src/PdfSharp/Drawing/CoreGraphicsPath.cs
+++ src/PdfSharp/Drawing/CoreGraphicsPath.cs
@@ -1,49 +1,17 @@
-﻿#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using PdfSharp.Internal;
 
 namespace PdfSharp.Drawing
 {
-    /// <summary>
-    /// Represents a graphics path that uses the same notation as GDI+.
-    /// </summary>
     internal class CoreGraphicsPath
     {
-        // Same values as GDI+ uses.
-        const byte PathPointTypeStart = 0;  // move
-        const byte PathPointTypeLine = 1;  // line
-        const byte PathPointTypeBezier = 3;  // default Bezier (= cubic Bezier)
-        const byte PathPointTypePathTypeMask = 0x07;  // type mask (lowest 3 bits).
-        const byte PathPointTypeCloseSubpath = 0x80;  // closed flag
+        const byte PathPointTypeStart = 0;   
+        const byte PathPointTypeLine = 1;   
+        const byte PathPointTypeBezier = 3;       
+        const byte PathPointTypePathTypeMask = 0x07;       
+        const byte PathPointTypeCloseSubpath = 0x80;    
 
         public CoreGraphicsPath()
         { }
@@ -56,8 +24,6 @@ namespace PdfSharp.Drawing
 
         public void MoveOrLineTo(double x, double y)
         {
-            // Make a MoveTo if there is no previous subpath or the previous subpath was closed.
-            // Otherwise make a LineTo.
             if (_types.Count == 0 || (_types[_types.Count - 1] & PathPointTypeCloseSubpath) == PathPointTypeCloseSubpath)
                 MoveTo(x, y);
             else
@@ -89,10 +55,6 @@ namespace PdfSharp.Drawing
             _types.Add((byte)(PathPointTypeBezier | (closeSubpath ? PathPointTypeCloseSubpath : 0)));
         }
 
-        /// <summary>
-        /// Adds an arc that fills exactly one quadrant (quarter) of an ellipse.
-        /// Just a quick hack to draw rounded rectangles before AddArc is fully implemented.
-        /// </summary>
         public void QuadrantArcTo(double x, double y, double width, double height, int quadrant, bool clockwise)
         {
             if (width < 0)
@@ -195,9 +157,6 @@ namespace PdfSharp.Drawing
             BezierTo(x1, y1, x2, y2, x3, y3, false);
         }
 
-        /// <summary>
-        /// Closes the current subpath.
-        /// </summary>
         public void CloseSubpath()
         {
             int count = _types.Count;
@@ -205,9 +164,6 @@ namespace PdfSharp.Drawing
                 _types[count - 1] |= PathPointTypeCloseSubpath;
         }
 
-        /// <summary>
-        /// Gets or sets the current fill mode (alternate or winding).
-        /// </summary>
         XFillMode FillMode
         {
             get { return _fillMode; }
@@ -249,33 +205,18 @@ namespace PdfSharp.Drawing
             MoveOrLineTo(points[0].X, points[0].Y);
             if (count == 2)
             {
-                //figure.Segments.Add(GeometryHelper.CreateCurveSegment(points[0], points[0], points[1], points[1], tension));
                 ToCurveSegment(points[0], points[0], points[1], points[1], tension);
             }
             else
             {
-                //figure.Segments.Add(GeometryHelper.CreateCurveSegment(points[0], points[0], points[1], points[2], tension));
                 ToCurveSegment(points[0], points[0], points[1], points[2], tension);
                 for (int idx = 1; idx < count - 2; idx++)
                 {
-                    //figure.Segments.Add(GeometryHelper.CreateCurveSegment(points[idx - 1], points[idx], points[idx + 1], points[idx + 2], tension));
                     ToCurveSegment(points[idx - 1], points[idx], points[idx + 1], points[idx + 2], tension);
                 }
-                //figure.Segments.Add(GeometryHelper.CreateCurveSegment(points[count - 3], points[count - 2], points[count - 1], points[count - 1], tension));
                 ToCurveSegment(points[count - 3], points[count - 2], points[count - 1], points[count - 1], tension);
             }
         }
-
-        ///// <summary>
-        ///// Appends a Bézier curve for a cardinal spline through pt1 and pt2.
-        ///// </summary>
-        //void ToCurveSegment(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3, double tension3, bool closeSubpath)
-        //{
-        //    BezierTo(
-        //        x1 + tension3 * (x2 - x0), y1 + tension3 * (y2 - y0),
-        //        x2 - tension3 * (x3 - x1), y2 - tension3 * (y3 - y1),
-        //        x2, y2, closeSubpath);
-        //}
 
         void ToCurveSegment(XPoint pt0, XPoint pt1, XPoint pt2, XPoint pt3, double tension3)
         {
@@ -286,14 +227,8 @@ namespace PdfSharp.Drawing
                 false);
         }
 
-        /// <summary>
-        /// Gets the path points in GDI+ style.
-        /// </summary>
         public XPoint[] PathPoints { get { return _points.ToArray(); } }
 
-        /// <summary>
-        /// Gets the path types in GDI+ style.
-        /// </summary>
         public byte[] PathTypes { get { return _types.ToArray(); } }
 
         readonly List<XPoint> _points = new List<XPoint>();
