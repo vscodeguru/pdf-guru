200a6987c46fbed82caa386aab9c8c13e83c878a
jones <ansolamjones1902@gmail.com>
2022-06-10 21:24:58 +0530

    Command Removed

--- src/PdfSharp/Pdf/PdfInteger.cs
+++ src/PdfSharp/Pdf/PdfInteger.cs
@@ -1,32 +1,3 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 using System.Diagnostics;
 using System.Globalization;
@@ -34,54 +5,32 @@ using PdfSharp.Pdf.IO;
 
 namespace PdfSharp.Pdf
 {
-    /// <summary>
-    /// Represents a direct integer value.
-    /// </summary>
     [DebuggerDisplay("({Value})")]
     public sealed class PdfInteger : PdfNumber, IConvertible
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfInteger"/> class.
-        /// </summary>
         public PdfInteger()
         { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfInteger"/> class.
-        /// </summary>
-        /// <param name="value">The value.</param>
         public PdfInteger(int value)
         {
             _value = value;
         }
 
-        /// <summary>
-        /// Gets the value as integer.
-        /// </summary>
         public int Value
         {
-            // This class must behave like a value type. Therefore it cannot be changed (like System.String).
             get { return _value; }
         }
         readonly int _value;
 
-        /// <summary>
-        /// Returns the integer as string.
-        /// </summary>
         public override string ToString()
         {
             return _value.ToString(CultureInfo.InvariantCulture);
         }
 
-        /// <summary>
-        /// Writes the integer as string.
-        /// </summary>
         internal override void WriteObject(PdfWriter writer)
         {
             writer.Write(this);
         }
-
-        #region IConvertible Members
 
         ulong IConvertible.ToUInt64(IFormatProvider provider)
         {
@@ -100,7 +49,6 @@ namespace PdfSharp.Pdf
 
         DateTime IConvertible.ToDateTime(IFormatProvider provider)
         {
-            // TODO:  Add PdfInteger.ToDateTime implementation
             return new DateTime();
         }
 
@@ -149,9 +97,6 @@ namespace PdfSharp.Pdf
             return _value;
         }
 
-        /// <summary>
-        /// Returns TypeCode for 32-bit integers.
-        /// </summary>
         public TypeCode GetTypeCode()
         {
             return TypeCode.Int32;
@@ -164,7 +109,6 @@ namespace PdfSharp.Pdf
 
         object IConvertible.ToType(Type conversionType, IFormatProvider provider)
         {
-            // TODO:  Add PdfInteger.ToType implementation
             return null;
         }
 
@@ -173,6 +117,5 @@ namespace PdfSharp.Pdf
             return Convert.ToUInt32(_value);
         }
 
-        #endregion
     }
 }
