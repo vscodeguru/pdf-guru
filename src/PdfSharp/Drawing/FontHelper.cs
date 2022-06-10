a9f2fd3dd890cf35ae7accd408c4b86b12e24418
jones <ansolamjones1902@gmail.com>
2022-06-10 23:19:43 +0530

    Command Removed

--- src/PdfSharp/Drawing/FontHelper.cs
+++ src/PdfSharp/Drawing/FontHelper.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
-#if CORE || GDI
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using GdiFontFamily = System.Drawing.FontFamily;
 using GdiFont = System.Drawing.Font;
 using GdiFontStyle = System.Drawing.FontStyle;
-#endif
 using PdfSharp.Fonts;
 using PdfSharp.Fonts.OpenType;
 
