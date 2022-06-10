6baadd0cd31beefe4ec38362fd140e8428884ef7
jones <ansolamjones1902@gmail.com>
2022-06-10 20:53:11 +0530

    Command Removed

--- src/PdfSharp/Internal/NativeMethods.cs
+++ src/PdfSharp/Internal/NativeMethods.cs
@@ -1,55 +1,16 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 using System.Runtime.InteropServices;
-
-// ReSharper disable InconsistentNaming
 
 namespace PdfSharp.Internal
 {
 #if CORE || GDI || WPF
-    /// <summary>
-    /// Required native Win32 calls.
-    /// </summary>
     static class NativeMethods
     {
         public const int GDI_ERROR = -1;
 
-        /// <summary>
-        /// Reflected from System.Drawing.SafeNativeMethods+LOGFONT
-        /// </summary>
-        //[SuppressUnmanagedCodeSecurity]
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
         public class LOGFONT
         {
-            // Preserve us for warning CS0649...
             LOGFONT(int dummy)
             {
                 lfHeight = 0;
@@ -114,14 +75,13 @@ namespace PdfSharp.Internal
 
         [DllImport("gdi32.dll", SetLastError = true)]
         public static extern int GetFontData(
-            IntPtr hdc, // handle to DC
-            uint dwTable, // metric table name
-            uint dwOffset, // offset into table
-            byte[] lpvBuffer, // buffer for returned data
-            int cbData // length of data
+            IntPtr hdc,    
+            uint dwTable,    
+            uint dwOffset,    
+            byte[] lpvBuffer,     
+            int cbData    
             );
 
-        //  CreateDCA(__in_opt LPCSTR pwszDriver, __in_opt LPCSTR pwszDevice, __in_opt LPCSTR pszPort, __in_opt CONST DEVMODEA* pdm);
         [DllImport("gdi32.dll", SetLastError = true)]
         public static extern IntPtr CreateDC(
             string driver,
@@ -142,12 +102,12 @@ namespace PdfSharp.Internal
         [DllImport("gdi32.dll")]
         public static extern bool DeleteObject(IntPtr hgdiobj);
 
-        public const int HORZSIZE = 4; // Horizontal size in millimeters
-        public const int VERTSIZE = 6; // Vertical size in millimeters
-        public const int HORZRES = 8; // Horizontal width in pixels
-        public const int VERTRES = 10; // Vertical height in pixels
-        public const int LOGPIXELSX = 88; // Logical pixels/inch in X
-        public const int LOGPIXELSY = 90; // Logical pixels/inch in Y
+        public const int HORZSIZE = 4;     
+        public const int VERTSIZE = 6;     
+        public const int HORZRES = 8;     
+        public const int VERTRES = 10;     
+        public const int LOGPIXELSX = 88;     
+        public const int LOGPIXELSY = 90;     
         [DllImport("gdi32.dll")]
         public static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
     }
