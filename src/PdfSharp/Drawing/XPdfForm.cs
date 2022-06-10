7f1337d55397baad9f92683dca975a4b068d6e7f
jones <ansolamjones1902@gmail.com>
2022-06-10 19:04:45 +0530

    Command Removed

--- src/PdfSharp/Drawing/XPdfForm.cs
+++ src/PdfSharp/Drawing/XPdfForm.cs
@@ -1,64 +1,13 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 using System.IO;
-#if GDI
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-#endif
-#if WPF
-using System.Windows.Media;
-#endif
 using PdfSharp.Internal;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
 
 namespace PdfSharp.Drawing
 {
-    /// <summary>
-    /// Represents a so called 'PDF form external object', which is typically an imported page of an external
-    /// PDF document. XPdfForm objects are used like images to draw an existing PDF page of an external
-    /// document in the current document. XPdfForm objects can only be placed in PDF documents. If you try
-    /// to draw them using a XGraphics based on an GDI+ context no action is taken if no placeholder image
-    /// is specified. Otherwise the place holder is drawn.
-    /// </summary>
     public class XPdfForm : XForm
     {
-        /// <summary>
-        /// Initializes a new instance of the XPdfForm class from the specified path to an external PDF document.
-        /// Although PDFsharp internally caches XPdfForm objects it is recommended to reuse XPdfForm objects
-        /// in your code and change the PageNumber property if more than one page is needed form the external
-        /// document. Furthermore, because XPdfForm can occupy very much memory, it is recommended to
-        /// dispose XPdfForm objects if not needed anymore.
-        /// </summary>
         internal XPdfForm(string path)
         {
             int pageNumber;
@@ -78,13 +27,8 @@ namespace PdfSharp.Drawing
                 PageNumber = pageNumber;
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="XPdfForm"/> class from a stream.
-        /// </summary>
-        /// <param name="stream">The stream.</param>
         internal XPdfForm(Stream stream)
         {
-            // Create a dummy unique path
             _path = "*" + Guid.NewGuid().ToString("B");
 
             if (PdfReader.TestPdfFile(stream) == 0)
@@ -93,33 +37,16 @@ namespace PdfSharp.Drawing
             _externalDocument = PdfReader.Open(stream);
         }
 
-        /// <summary>
-        /// Creates an XPdfForm from a file.
-        /// </summary>
         public static new XPdfForm FromFile(string path)
         {
-            // TODO: Same file should return same object (that's why the function is static).
             return new XPdfForm(path);
         }
 
-        /// <summary>
-        /// Creates an XPdfForm from a stream.
-        /// </summary>
         public static new XPdfForm FromStream(Stream stream)
         {
             return new XPdfForm(stream);
         }
 
-        /*
-            void Initialize()
-            {
-              // ImageFormat has no overridden Equals...
-            }
-        */
-
-        /// <summary>
-        /// Sets the form in the state FormState.Finished.
-        /// </summary>
         internal override void Finish()
         {
             if (_formState == FormState.NotATemplate || _formState == FormState.Finished)
@@ -127,34 +54,8 @@ namespace PdfSharp.Drawing
 
             base.Finish();
 
-            //if (Gfx.metafile != null)
-            //  image = Gfx.metafile;
-
-            //Debug.Assert(_fromState == FormState.Created || _fromState == FormState.UnderConstruction);
-            //_fromState = FormState.Finished;
-            //Gfx.Dispose();
-            //Gfx = null;
-
-            //if (_pdfRenderer != null)
-            //{
-            //  _pdfForm.Stream = new PdfDictionary.PdfStream(PdfEncoders.RawEncoding.GetBytes(pdfRenderer.GetContent()), this.pdfForm);
-
-            //  if (_document.Options.CompressContentStreams)
-            //  {
-            //    _pdfForm.Stream.Value = Filtering.FlateDecode.Encode(pdfForm.Stream.Value);
-            //    _pdfForm.Elements["/Filter"] = new PdfName("/FlateDecode");
-            //  }
-            //  int length = _pdfForm.Stream.Length;
-            //  _pdfForm.Elements.SetInteger("/Length", length);
-            //}
         }
 
-        /// <summary>
-        /// Frees the memory occupied by the underlying imported PDF document, even if other XPdfForm objects
-        /// refer to this document. A reuse of this object doesn't fail, because the underlying PDF document
-        /// is re-imported if necessary.
-        /// </summary>
-        // TODO: NYI: Dispose
         protected override void Dispose(bool disposing)
         {
             if (!_disposed)
@@ -164,11 +65,9 @@ namespace PdfSharp.Drawing
                 {
                     if (disposing)
                     {
-                        //...
                     }
                     if (_externalDocument != null)
                         PdfDocument.Tls.DetachDocument(_externalDocument.Handle);
-                    //...
                 }
                 finally
                 {
@@ -178,11 +77,6 @@ namespace PdfSharp.Drawing
         }
         bool _disposed;
 
-        /// <summary>
-        /// Gets or sets an image that is used for drawing if the current XGraphics object cannot handle
-        /// PDF forms. A place holder is useful for showing a preview of a page on the display, because
-        /// PDFsharp cannot render native PDF objects.
-        /// </summary>
         public XImage PlaceHolder
         {
             get { return _placeHolder; }
@@ -190,9 +84,6 @@ namespace PdfSharp.Drawing
         }
         XImage _placeHolder;
 
-        /// <summary>
-        /// Gets the underlying PdfPage (if one exists).
-        /// </summary>
         public PdfPage Page
         {
             get
@@ -204,9 +95,6 @@ namespace PdfSharp.Drawing
             }
         }
 
-        /// <summary>
-        /// Gets the number of pages in the PDF form.
-        /// </summary>
         public int PageCount
         {
             get
@@ -220,9 +108,6 @@ namespace PdfSharp.Drawing
         }
         int _pageCount = -1;
 
-        /// <summary>
-        /// Gets the width in point of the page identified by the property PageNumber.
-        /// </summary>
         [Obsolete("Use either PixelWidth or PointWidth. Temporarily obsolete because of rearrangements for WPF.")]
         public override double Width
         {
@@ -233,9 +118,6 @@ namespace PdfSharp.Drawing
             }
         }
 
-        /// <summary>
-        /// Gets the height in point of the page identified by the property PageNumber.
-        /// </summary>
         [Obsolete("Use either PixelHeight or PointHeight. Temporarily obsolete because of rearrangements for WPF.")]
         public override double Height
         {
@@ -246,9 +128,6 @@ namespace PdfSharp.Drawing
             }
         }
 
-        /// <summary>
-        /// Gets the width in point of the page identified by the property PageNumber.
-        /// </summary>
         public override double PointWidth
         {
             get
@@ -258,9 +137,6 @@ namespace PdfSharp.Drawing
             }
         }
 
-        /// <summary>
-        /// Gets the height in point of the page identified by the property PageNumber.
-        /// </summary>
         public override double PointHeight
         {
             get
@@ -270,35 +146,22 @@ namespace PdfSharp.Drawing
             }
         }
 
-        /// <summary>
-        /// Gets the width in point of the page identified by the property PageNumber.
-        /// </summary>
         public override int PixelWidth
         {
             get
             {
-                //PdfPage page = ExternalDocument.Pages[_pageNumber - 1];
-                //return (int)page.Width;
                 return DoubleUtil.DoubleToInt(PointWidth);
             }
         }
 
-        /// <summary>
-        /// Gets the height in point of the page identified by the property PageNumber.
-        /// </summary>
         public override int PixelHeight
         {
             get
             {
-                //PdfPage page = ExternalDocument.Pages[_pageNumber - 1];
-                //return (int)page.Height;
                 return DoubleUtil.DoubleToInt(PointHeight);
             }
         }
 
-        /// <summary>
-        /// Get the size of the page identified by the property PageNumber.
-        /// </summary>
         public override XSize Size
         {
             get
@@ -308,9 +171,6 @@ namespace PdfSharp.Drawing
             }
         }
 
-        /// <summary>
-        /// Gets or sets the transformation matrix.
-        /// </summary>
         public override XMatrix Transform
         {
             get { return _transform; }
@@ -318,17 +178,12 @@ namespace PdfSharp.Drawing
             {
                 if (_transform != value)
                 {
-                    // discard PdfFromXObject when Transform changed
                     _pdfForm = null;
                     _transform = value;
                 }
             }
         }
 
-        /// <summary>
-        /// Gets or sets the page number in the external PDF document this object refers to. The page number
-        /// is one-based, i.e. it is in the range from 1 to PageCount. The default value is 1.
-        /// </summary>
         public int PageNumber
         {
             get { return _pageNumber; }
@@ -340,34 +195,20 @@ namespace PdfSharp.Drawing
                 if (_pageNumber != value)
                 {
                     _pageNumber = value;
-                    // dispose PdfFromXObject when number has changed
                     _pdfForm = null;
                 }
             }
         }
         int _pageNumber = 1;
 
-        /// <summary>
-        /// Gets or sets the page index in the external PDF document this object refers to. The page index
-        /// is zero-based, i.e. it is in the range from 0 to PageCount - 1. The default value is 0.
-        /// </summary>
         public int PageIndex
         {
             get { return PageNumber - 1; }
             set { PageNumber = value + 1; }
         }
 
-        /// <summary>
-        /// Gets the underlying document from which pages are imported.
-        /// </summary>
         internal PdfDocument ExternalDocument
         {
-            // The problem is that you can ask an XPdfForm about the number of its pages before it was
-            // drawn the first time. At this moment the XPdfForm doesn't know the document where it will
-            // be later draw on one of its pages. To prevent the import of the same document more than
-            // once, all imported documents of a thread are cached. The cache is local to the current 
-            // thread and not to the appdomain, because I won't get problems in a multi-thread environment
-            // that I don't understand.
             get
             {
                 if (IsTemplate)
@@ -380,10 +221,6 @@ namespace PdfSharp.Drawing
         }
         internal PdfDocument _externalDocument;
 
-        /// <summary>
-        /// Extracts the page number if the path has the form 'MyFile.pdf#123' and returns
-        /// the actual path without the number sign and the following digits.
-        /// </summary>
         public static string ExtractPageNumber(string path, out int pageNumber)
         {
             if (path == null)
@@ -400,7 +237,6 @@ namespace PdfSharp.Drawing
                         length--;
                     if (length > 0 && path[length] == '#')
                     {
-                        // Must have at least one dot left of colon to distinguish from e.g. '#123'
                         if (path.IndexOf('.') != -1)
                         {
                             pageNumber = int.Parse(path.Substring(length + 1));
--- src/PdfSharp/Drawing/XPen.cs
+++ src/PdfSharp/Drawing/XPen.cs
@@ -1,66 +1,14 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
 using PdfSharp.Internal;
-#if GDI
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using GdiPen = System.Drawing.Pen;
-#endif
-#if WPF
-using System.Windows;
-using System.Windows.Media;
-using WpfPen =System.Windows.Media.Pen;
-using WpfBrush =System.Windows.Media.Brush;
-#endif
-#if UWP
-#endif
 
 namespace PdfSharp.Drawing
 {
-    // TODO Free GDI objects (pens, brushes, ...) automatically without IDisposable.
-    /// <summary>
-    /// Defines an object used to draw lines and curves.
-    /// </summary>
     public sealed class XPen
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="XPen"/> class.
-        /// </summary>
         public XPen(XColor color)
             : this(color, 1, false)
         { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="XPen"/> class.
-        /// </summary>
         public XPen(XColor color, double width)
             : this(color, width, false)
         { }
@@ -76,9 +24,6 @@ namespace PdfSharp.Drawing
             _immutable = immutable;
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="XPen"/> class.
-        /// </summary>
         public XPen(XPen pen)
         {
             _color = pen._color;
@@ -92,17 +37,11 @@ namespace PdfSharp.Drawing
                 _dashPattern = (double[])_dashPattern.Clone();
         }
 
-        /// <summary>
-        /// Clones this instance.
-        /// </summary>
         public XPen Clone()
         {
             return new XPen(this);
         }
 
-        /// <summary>
-        /// Gets or sets the color.
-        /// </summary>
         public XColor Color
         {
             get { return _color; }
@@ -116,9 +55,6 @@ namespace PdfSharp.Drawing
         }
         internal XColor _color;
 
-        /// <summary>
-        /// Gets or sets the width.
-        /// </summary>
         public double Width
         {
             get { return _width; }
@@ -132,9 +68,6 @@ namespace PdfSharp.Drawing
         }
         internal double _width;
 
-        /// <summary>
-        /// Gets or sets the line join.
-        /// </summary>
         public XLineJoin LineJoin
         {
             get { return _lineJoin; }
@@ -148,9 +81,6 @@ namespace PdfSharp.Drawing
         }
         internal XLineJoin _lineJoin;
 
-        /// <summary>
-        /// Gets or sets the line cap.
-        /// </summary>
         public XLineCap LineCap
         {
             get { return _lineCap; }
@@ -164,9 +94,6 @@ namespace PdfSharp.Drawing
         }
         internal XLineCap _lineCap;
 
-        /// <summary>
-        /// Gets or sets the miter limit.
-        /// </summary>
         public double MiterLimit
         {
             get { return _miterLimit; }
@@ -180,9 +107,6 @@ namespace PdfSharp.Drawing
         }
         internal double _miterLimit;
 
-        /// <summary>
-        /// Gets or sets the dash style.
-        /// </summary>
         public XDashStyle DashStyle
         {
             get { return _dashStyle; }
@@ -196,9 +120,6 @@ namespace PdfSharp.Drawing
         }
         internal XDashStyle _dashStyle;
 
-        /// <summary>
-        /// Gets or sets the dash offset.
-        /// </summary>
         public double DashOffset
         {
             get { return _dashOffset; }
@@ -212,9 +133,6 @@ namespace PdfSharp.Drawing
         }
         internal double _dashOffset;
 
-        /// <summary>
-        /// Gets or sets the dash pattern.
-        /// </summary>
         public double[] DashPattern
         {
             get
@@ -229,9 +147,6 @@ namespace PdfSharp.Drawing
                     throw new ArgumentException(PSSR.CannotChangeImmutableObject("XPen"));
 
                 int length = value.Length;
-                //if (length == 0)
-                //  throw new ArgumentException("Dash pattern array must not be empty.");
-
                 for (int idx = 0; idx < length; idx++)
                 {
                     if (value[idx] <= 0)
@@ -245,10 +160,6 @@ namespace PdfSharp.Drawing
         }
         internal double[] _dashPattern;
 
-        /// <summary>
-        /// Gets or sets a value indicating whether the pen enables overprint when used in a PDF document.
-        /// Experimental, takes effect only on CMYK color mode.
-        /// </summary>
         public bool Overprint
         {
             get { return _overprint; }
@@ -261,144 +172,8 @@ namespace PdfSharp.Drawing
         }
         internal bool _overprint;
 
-#if GDI
-#if UseGdiObjects
-        /// <summary>
-        /// Implicit conversion from Pen to XPen
-        /// </summary>
-        public static implicit operator XPen(Pen pen)
-        {
-            XPen xpen;
-            try
-            {
-                Lock.EnterGdiPlus();
-                switch (pen.PenType)
-                {
-                    case PenType.SolidColor:
-                        xpen = new XPen(pen.Color, pen.Width);
-                        xpen.LineJoin = (XLineJoin)pen.LineJoin;
-                        xpen.DashStyle = (XDashStyle)pen.DashStyle;
-                        xpen._miterLimit = pen.MiterLimit;
-                        break;
-
-                    default:
-                        throw new NotImplementedException("Pen type not supported by PDFsharp.");
-                }
-                // Bug fixed by drice2@ageone.de
-                if (pen.DashStyle == System.Drawing.Drawing2D.DashStyle.Custom)
-                {
-                    int length = pen.DashPattern.Length;
-                    double[] pattern = new double[length];
-                    for (int idx = 0; idx < length; idx++)
-                        pattern[idx] = pen.DashPattern[idx];
-                    xpen.DashPattern = pattern;
-                    xpen._dashOffset = pen.DashOffset;
-                }
-            }
-            finally { Lock.ExitGdiPlus(); }
-            return xpen;
-        }
-#endif
-
-        internal System.Drawing.Pen RealizeGdiPen()
-        {
-            if (_dirty)
-            {
-                if (_gdiPen == null)
-                    _gdiPen = new System.Drawing.Pen(_color.ToGdiColor(), (float)_width);
-                else
-                {
-                    _gdiPen.Color = _color.ToGdiColor();
-                    _gdiPen.Width = (float)_width;
-                }
-                LineCap lineCap = XConvert.ToLineCap(_lineCap);
-                _gdiPen.StartCap = lineCap;
-                _gdiPen.EndCap = lineCap;
-                _gdiPen.LineJoin = XConvert.ToLineJoin(_lineJoin);
-                _gdiPen.DashOffset = (float)_dashOffset;
-                if (_dashStyle == XDashStyle.Custom)
-                {
-                    int len = _dashPattern == null ? 0 : _dashPattern.Length;
-                    float[] pattern = new float[len];
-                    for (int idx = 0; idx < len; idx++)
-                        pattern[idx] = (float)_dashPattern[idx];
-                    _gdiPen.DashPattern = pattern;
-                }
-                else
-                    _gdiPen.DashStyle = (System.Drawing.Drawing2D.DashStyle)_dashStyle;
-            }
-            return _gdiPen;
-        }
-#endif
-
-#if WPF
-        internal WpfPen RealizeWpfPen()
-        {
-#if !SILVERLIGHT
-            if (_dirty || !_dirty) // TODOWPF: XPen is frozen by design, WPF Pen can change
-            {
-                //if (_wpfPen == null)
-                _wpfPen = new WpfPen(new SolidColorBrush(_color.ToWpfColor()), _width);
-                //else
-                //{
-                //  _wpfPen.Brush = new SolidColorBrush(_color.ToWpfColor());
-                //  _wpfPen.Thickness = _width;
-                //}
-                PenLineCap lineCap = XConvert.ToPenLineCap(_lineCap);
-                _wpfPen.StartLineCap = lineCap;
-                _wpfPen.EndLineCap = lineCap;
-                _wpfPen.LineJoin = XConvert.ToPenLineJoin(_lineJoin);
-                if (_dashStyle == XDashStyle.Custom)
-                {
-                    // TODOWPF: does not work in all cases
-                    _wpfPen.DashStyle = new System.Windows.Media.DashStyle(_dashPattern, _dashOffset);
-                }
-                else
-                {
-                    switch (_dashStyle)
-                    {
-                        case XDashStyle.Solid:
-                            _wpfPen.DashStyle = DashStyles.Solid;
-                            break;
-
-                        case XDashStyle.Dash:
-                            //_wpfPen.DashStyle = DashStyles.Dash;
-                            _wpfPen.DashStyle = new System.Windows.Media.DashStyle(new double[] { 2, 2 }, 0);
-                            break;
-
-                        case XDashStyle.Dot:
-                            //_wpfPen.DashStyle = DashStyles.Dot;
-                            _wpfPen.DashStyle = new System.Windows.Media.DashStyle(new double[] { 0, 2 }, 1.5);
-                            break;
-
-                        case XDashStyle.DashDot:
-                            //_wpfPen.DashStyle = DashStyles.DashDot;
-                            _wpfPen.DashStyle = new System.Windows.Media.DashStyle(new double[] { 2, 2, 0, 2 }, 0);
-                            break;
-
-                        case XDashStyle.DashDotDot:
-                            //_wpfPen.DashStyle = DashStyles.DashDotDot;
-                            _wpfPen.DashStyle = new System.Windows.Media.DashStyle(new double[] { 2, 2, 0, 2, 0, 2 }, 0);
-                            break;
-                    }
-                }
-            }
-#else
-      _wpfPen = new System.Windows.Media.Pen();
-      _wpfPen.Brush = new SolidColorBrush(_color.ToWpfColor());
-      _wpfPen.Thickness = _width;
-#endif
-            return _wpfPen;
-        }
-#endif
-
         bool _dirty = true;
         readonly bool _immutable;
-#if GDI
-        GdiPen _gdiPen;
-#endif
-#if WPF
-        WpfPen _wpfPen;
-#endif
+
     }
 }
