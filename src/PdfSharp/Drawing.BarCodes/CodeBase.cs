2b47f7cf514d2d4e5a8e9e81e99d220f9254cfe0
jones <ansolamjones1902@gmail.com>
2022-06-10 19:55:25 +0530

    Command Removed

--- src/PdfSharp/Drawing.BarCodes/CodeBase.cs
+++ src/PdfSharp/Drawing.BarCodes/CodeBase.cs
@@ -1,42 +1,7 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Klaus Potzesny
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 namespace PdfSharp.Drawing.BarCodes
 {
-    /// <summary>
-    /// Represents the base class of all codes.
-    /// </summary>
     public abstract class CodeBase
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CodeBase"/> class.
-        /// </summary>
         public CodeBase(string text, XSize size, CodeDirection direction)
         {
             _text = text;
@@ -44,39 +9,6 @@ namespace PdfSharp.Drawing.BarCodes
             _direction = direction;
         }
 
-        //public static CodeBase FromType(CodeType type, string text, XSize size, CodeDirection direction)
-        //{
-        //  switch (type)
-        //  {
-        //    case CodeType.Code2of5Interleaved:
-        //      return new Code2of5Interleaved(text, size, direction);
-
-        //    case CodeType.Code3of9Standard:
-        //      return new Code3of9Standard(text, size, direction);
-
-        //    default:
-        //      throw new InvalidEnumArgumentException("type", (int)type, typeof(CodeType));
-        //  }
-        //}
-
-        //public static CodeBase FromType(CodeType type, string text, XSize size)
-        //{
-        //  return FromType(type, text, size, CodeDirection.LeftToRight);
-        //}
-
-        //public static CodeBase FromType(CodeType type, string text)
-        //{
-        //  return FromType(type, text, XSize.Empty, CodeDirection.LeftToRight);
-        //}
-
-        //public static CodeBase FromType(CodeType type)
-        //{
-        //  return FromType(type, String.Empty, XSize.Empty, CodeDirection.LeftToRight);
-        //}
-
-        /// <summary>
-        /// Gets or sets the size.
-        /// </summary>
         public XSize Size
         {
             get { return _size; }
@@ -84,9 +16,6 @@ namespace PdfSharp.Drawing.BarCodes
         }
         XSize _size;
 
-        /// <summary>
-        /// Gets or sets the text the bar code shall represent.
-        /// </summary>
         public string Text
         {
             get { return _text; }
@@ -98,9 +27,6 @@ namespace PdfSharp.Drawing.BarCodes
         }
         string _text;
 
-        /// <summary>
-        /// Always MiddleCenter.
-        /// </summary>
         public AnchorType Anchor
         {
             get { return _anchor; }
@@ -108,9 +34,6 @@ namespace PdfSharp.Drawing.BarCodes
         }
         AnchorType _anchor;
 
-        /// <summary>
-        /// Gets or sets the drawing direction.
-        /// </summary>
         public CodeDirection Direction
         {
             get { return _direction; }
@@ -118,20 +41,8 @@ namespace PdfSharp.Drawing.BarCodes
         }
         CodeDirection _direction;
 
-        /// <summary>
-        /// When implemented in a derived class, determines whether the specified string can be used as Text
-        /// for this bar code type.
-        /// </summary>
-        /// <param name="text">The code string to check.</param>
-        /// <returns>True if the text can be used for the actual barcode.</returns>
         protected abstract void CheckCode(string text);
 
-        /// <summary>
-        /// Calculates the distance between an old anchor point and a new anchor point.
-        /// </summary>
-        /// <param name="oldType"></param>
-        /// <param name="newType"></param>
-        /// <param name="size"></param>
         public static XVector CalcDistance(AnchorType oldType, AnchorType newType, XSize size)
         {
             if (oldType == newType)
