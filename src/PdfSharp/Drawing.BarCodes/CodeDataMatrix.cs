8139432d8739356baef7d096396e9c2bf6fc3934
jones <ansolamjones1902@gmail.com>
2022-06-10 19:56:06 +0530

    Command Removed

--- src/PdfSharp/Drawing.BarCodes/CodeDataMatrix.cs
+++ src/PdfSharp/Drawing.BarCodes/CodeDataMatrix.cs
@@ -1,118 +1,47 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   David Stephensen
-//   Stefan Lange
-//
-// Copyright (c) 2005-2017 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.pdfsharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
-
 using System;
-#if GDI
-using System.Drawing;
-#endif
-#if WPF
-using System.Windows;
-using System.Windows.Media;
-#endif
 
 namespace PdfSharp.Drawing.BarCodes
 {
-    /// <summary>
-    /// Defines the DataMatrix 2D barcode. THIS IS AN EMPIRA INTERNAL IMPLEMENTATION. THE CODE IN
-    /// THE OPEN SOURCE VERSION IS A FAKE.
-    /// </summary>
     public class CodeDataMatrix : MatrixCode
     {
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix()
             : this("", "", 26, 26, 0, XSize.Empty)
         {}
 
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix(string code, int length)
             : this(code, "", length, length, 0, XSize.Empty)
         {}
 
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix(string code, int length, XSize size)
             : this(code, "", length, length, 0, size)
         {}
 
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix(string code, DataMatrixEncoding dmEncoding, int length, XSize size)
             : this(code, CreateEncoding(dmEncoding, code.Length), length, length, 0, size)
         {}
 
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix(string code, int rows, int columns)
             : this(code, "", rows, columns, 0, XSize.Empty)
         {}
 
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix(string code, int rows, int columns, XSize size)
             : this(code, "", rows, columns, 0, size)
         {}
 
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix(string code, DataMatrixEncoding dmEncoding, int rows, int columns, XSize size)
             : this(code, CreateEncoding(dmEncoding, code.Length), rows, columns, 0, size)
         {}
 
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix(string code, int rows, int columns, int quietZone)
             : this(code, "", rows, columns, quietZone, XSize.Empty)
         {}
 
-        /// <summary>
-        /// Initializes a new instance of CodeDataMatrix.
-        /// </summary>
         public CodeDataMatrix(string code, string encoding, int rows, int columns, int quietZone, XSize size)
             : base(code, encoding, rows, columns, size)
         {
             QuietZone = quietZone;
         }
 
-        /// <summary>
-        /// Sets the encoding of the DataMatrix.
-        /// </summary>
         public void SetEncoding(DataMatrixEncoding dmEncoding)
         {
             Encoding = CreateEncoding(dmEncoding, Text.Length);
@@ -145,9 +74,6 @@ namespace PdfSharp.Drawing.BarCodes
             return tempencoding;
         }
 
-        /// <summary>
-        /// Gets or sets the size of the Matrix' Quiet Zone.
-        /// </summary>
         public int QuietZone
         {
             get { return _quietZone; }
@@ -155,9 +81,6 @@ namespace PdfSharp.Drawing.BarCodes
         }
         int _quietZone;
 
-        /// <summary>
-        /// Renders the matrix code.
-        /// </summary>
         protected internal override void Render(XGraphics gfx, XBrush brush, XPoint position)
         {
             XGraphicsState state = gfx.Save();
@@ -201,10 +124,6 @@ namespace PdfSharp.Drawing.BarCodes
             gfx.Restore(state);
         }
 
-        /// <summary>
-        /// Determines whether the specified string can be used as data in the DataMatrix.
-        /// </summary>
-        /// <param name="text">The code to be checked.</param>
         protected override void CheckCode(string text)
         {
             if (text == null)
